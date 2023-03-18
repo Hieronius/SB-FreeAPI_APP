3d9f3068de98031e5a1493a5edba681688659adb
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-18 17:15:39 +0300

    Add newsDataStructure

---
+++ SB FreeAPI_App/SB FreeAPI_App/EldenRingData.swift
@@ -0,0 +1,23 @@
+//
+//  EldenRingData.swift
+//  SB FreeAPI_App
+//
+//  Created by Арсентий Халимовский on 18.03.2023.
+//
+
+import Foundation
+
+
+struct Article: Codable {
+    let id: String?
+    let type: String?
+    let sectionId: String?
+    let sectionName: String?
+    let webPublicationDate: String?
+    let webTitle: String?
+    let webUrl: String?
+    let apiUrl: String?
+    var isHosted: Bool = false
+    let pillarId: String
+    let pillarName: String
+}
---
+++ SB FreeAPI_App/SB FreeAPI_App/NewsAPIMaster.swift
@@ -0,0 +1,18 @@
+//
+//  NewsAPIMaster.swift
+//  SB FreeAPI_App
+//
+//  Created by Арсентий Халимовский on 18.03.2023.
+//
+
+import Foundation
+
+class NewsAPIMaster {
+    
+    static let shared = NewsAPIMaster()
+    let urlString = "https://content.guardianapis.com/search?q=Elden%20ring&api-key=580e6ccc-7e24-46f2-be59-e6ba0e86765b"
+    
+    
+    // func getNewsAboutElderRing(completion: @escaping (
+    
+}
