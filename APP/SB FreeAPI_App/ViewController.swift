e2b575798587b9538176e4fcd54f9044d73c8021
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-22 17:59:20 +0300

    Import Alamofire

--- APP/SB FreeAPI_App/ViewController.swift
+++ APP/SB FreeAPI_App/ViewController.swift
@@ -6,20 +6,31 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    let urlString = "https://content.guardianapis.com/search?q=Elden%20ring&api-key=580e6ccc-7e24-46f2-be59-e6ba0e86765b"
     
     var articles = [Result]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        NewsAPIMaster.shared.getNewsAboutElderRing { [weak self] values in
-            DispatchQueue.main.async {
-                guard let self else { return }
-                
-                self.articles = values
-                print(self.articles.count)
+//        // Do any additional setup after loading the view.
+//        NewsAPIMaster.shared.getNewsAboutElderRing { [weak self] values in
+//            DispatchQueue.main.async {
+//                guard let self else { return }
+//
+//                self.articles = values
+//                print(self.articles.count)
+//            }
+//        }
+        
+        
+        AF.request(urlString).validate().responseDecodable { dataResponse in
+            switch dataResponse.result {
+            case .success(let value):
+                self data = GuardianAPIData.
             }
         }
         
