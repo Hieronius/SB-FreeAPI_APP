09584bd1b7210b80ee5e89ebf3396c45743dd89c
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-25 14:19:37 +0300

    fix API Response

--- APP/SB FreeAPI_App/ViewController.swift
+++ APP/SB FreeAPI_App/ViewController.swift
@@ -16,23 +16,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//        NewsAPIMaster.shared.getNewsAboutElderRing { [weak self] values in
-//            DispatchQueue.main.async {
-//                guard let self else { return }
-//
-//                self.articles = values
-//                print(self.articles.count)
-//            }
-//        }
-        
-        
-        AF.request(urlString).validate().responseDecodable { dataResponse in
-            switch dataResponse.result {
-            case .success(let value):
-                self dataResponse = GuardianAPIData.
+        // Do any additional setup after loading the view.
+        NewsAPIMaster.shared.getNewsAboutElderRing { [weak self] values in
+            DispatchQueue.main.async {
+                guard let self else { return }
+
+                self.articles = values
+                print(self.articles.count)
             }
         }
+        
+        
+//        AF.request(urlString).validate().responseDecodable { dataResponse in
+//            switch dataResponse.result {
+//            case .success(let value):
+//                self dataResponse = GuardianAPIData.
+//            }
+//        }
         
     }
 
